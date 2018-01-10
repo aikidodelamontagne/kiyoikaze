05e5383fde8ebca4dfbad02909b8b48c03ce42c6
Jean-Rene David <info@aikidodelamontagne.com>
2018-01-09 23:09:46 -0500

    Initial commit

---
+++ es_media/es_media_files/es_media.js
@@ -0,0 +1,31 @@
+// Created by iWeb 3.0.4 local-build-20160130
+
+function createMediaStream_id1()
+{return IWCreateMediaCollection("http://www.kiyoikaze.org/Kiyoikaze/es_media/es_media_files/rss.xml",true,255,["No photos yet","%d photo","%d photos"],["","%d clip","%d clips"]);}
+function initializeMediaStream_id1()
+{createMediaStream_id1().load('http://www.kiyoikaze.org/Kiyoikaze/es_media',function(imageStream)
+{var entryCount=imageStream.length;var headerView=widgets['widget4'];headerView.setPreferenceForKey(imageStream.length,'entryCount');NotificationCenter.postNotification(new IWNotification('SetPage','id1',{pageIndex:0}));});}
+function layoutMediaGrid_id1(range)
+{createMediaStream_id1().load('http://www.kiyoikaze.org/Kiyoikaze/es_media',function(imageStream)
+{if(range==null)
+{range=new IWRange(0,imageStream.length);}
+IWLayoutPhotoGrid('id1',new IWPhotoGridLayout(3,new IWSize(251,188),new IWSize(251,32),new IWSize(313,235),27,27,0,new IWSize(18,19)),new IWPhotoFrame([IWCreateImage('es_media_files/ul.png'),IWCreateImage('es_media_files/top.png'),IWCreateImage('es_media_files/ur.png'),IWCreateImage('es_media_files/right.png'),IWCreateImage('es_media_files/lr.png'),IWCreateImage('es_media_files/bottom.png'),IWCreateImage('es_media_files/ll.png'),IWCreateImage('es_media_files/left.png')],null,0,0.333333,130.000000,10.000000,118.000000,45.000000,147.000000,30.000000,157.000000,83.000000,251.000000,120.000000,260.000000,120.000000,null,null,null,0.300000),imageStream,range,(null),null,1.000000,null,'../Media/slideshow.html','widget4',null,'widget5',{showTitle:true,showMetric:true})});}
+function relayoutMediaGrid_id1(notification)
+{var userInfo=notification.userInfo();var range=userInfo['range'];layoutMediaGrid_id1(range);}
+function onStubPage()
+{var args=window.location.href.toQueryParams();parent.IWMediaStreamPhotoPageSetMediaStream(createMediaStream_id1(),args.id);}
+if(window.stubPage)
+{onStubPage();}
+setTransparentGifURL('../Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{IWRegisterNamedImage('comment overlay','../Media/Photo-Overlay-Comment.png')
+IWRegisterNamedImage('movie overlay','../Media/Photo-Overlay-Movie.png')
+loadMozillaCSS('es_media_files/es_mediaMoz.css')
+NotificationCenter.addObserver(null,relayoutMediaGrid_id1,'RangeChanged','id1')
+adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');adjustLineHeightIfTooBig('id8');adjustFontSizeIfTooBig('id8');adjustLineHeightIfTooBig('id9');adjustFontSizeIfTooBig('id9');adjustLineHeightIfTooBig('id10');adjustFontSizeIfTooBig('id10');adjustLineHeightIfTooBig('id11');adjustFontSizeIfTooBig('id11');adjustLineHeightIfTooBig('id12');adjustFontSizeIfTooBig('id12');adjustLineHeightIfTooBig('id13');adjustFontSizeIfTooBig('id13');adjustLineHeightIfTooBig('id14');adjustFontSizeIfTooBig('id14');Widget.onload();fixAllIEPNGs('../Media/transparent.gif');applyEffects()
+initializeMediaStream_id1()}
+function onPageUnload()
+{Widget.onunload();}
